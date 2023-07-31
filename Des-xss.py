d492f9569d6c648f6b9fcfdb266b57c49aa368ad
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-07-31 05:16:42 +0200

    Update Des-xss.py

--- Des-xss.py
+++ Des-xss.py
@@ -31,6 +31,8 @@ def start():
 	pos_opt.add_argument("--depth",metavar="",help="Depth web page to crawl. Default: 2",default=2)
 	pos_opt.add_argument("--payload-level",metavar="",help="Level for payload Generator, 7 for custom payload. {1...6}. Default: 6",default=6)
 	pos_opt.add_argument("--payload",metavar="",help="Load custom payload directly (e.g. <script>alert(2005)</script>)",default=None)
+	pos_opt.add_argument("--method",metavar="",help="Method setting(s): \n\t0: GET\n\t1: POST\n\t2: GET and POST (default)",default=2,type=int)
+
 
 
 
