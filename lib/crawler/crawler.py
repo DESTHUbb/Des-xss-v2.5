5fb79fef73257f24568193366b0d3ddf30500726
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-08-02 00:29:48 +0200

    Create crawler.py

---
+++ lib/crawler/crawler.py
@@ -0,0 +1,7 @@
+import requests
+from lib.helper.Log import *
+from lib.helper.helper import *
+from lib.core import *
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from multiprocessing import Process
