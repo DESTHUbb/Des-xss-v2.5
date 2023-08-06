c58d77798a11e0c27376924cbbda333d0361b282
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-08-06 13:50:14 +0200

    Update core.py

--- lib/core.py
+++ lib/core.py
@@ -9,4 +9,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 class core:
 	
 	@classmethod
-	def generate(self,eff):		
+	def generate(self,eff):	
+		FUNCTION=[
+			"prompt(5000/200)",
+			"alert(6000/3000)",
