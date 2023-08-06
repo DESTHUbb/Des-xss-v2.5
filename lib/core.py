d4686f46a63f53044950bb47988cdd1004b0ed3b
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-08-06 13:49:34 +0200

    Update core.py

--- lib/core.py
+++ lib/core.py
@@ -1,1 +1,8 @@
+from lib.helper.helper import *
+from random import randint
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin,urlparse,parse_qs,urlencode
+from lib.helper.Log import *
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
