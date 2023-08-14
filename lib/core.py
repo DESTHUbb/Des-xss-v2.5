6daa9a117dae385c5e09bbbf92a137a93804a467
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-08-14 03:55:55 +0200

    Update core.py

--- lib/core.py
+++ lib/core.py
@@ -144,6 +144,7 @@ class core:
 					if not url.startswith("mailto:") and not url.startswith("tel:"):					
 						_respon=self.session.get(test,verify=False)
 						if self.payload in _respon.text or self.payload in self.session.get(query_all).text:
+							Log.high("Detected XSS (GET) at "+_respon.url)
 
 
 
